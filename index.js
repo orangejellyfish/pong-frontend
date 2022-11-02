953b8bf8685893c2432f74b0cd21b53777f2af49
James Allardice <james@allardice.me>
2022-11-02 15:55:30 +0000

    WIP: sockets

--- index.js
+++ index.js
@@ -1,3 +1,132 @@
+// WebSockets.
+const MIN_RECONNECT_DELAY = 250; // Min time to wait between reconnect attempts
+const RECONNECT_DECAY = 1.5; // Rate of increase of reconnect delay
+const MAX_RECONNECT_ATTEMPTS = 10;
+const CHECK_SOCKET_READY_INTERVAL = 10;
+
+// Messages sent and received are of the following format:
+//
+// {
+//   "action": "message_action"
+//   "data": {
+//     "foo": "bar"
+//   }
+// }
+//
+
+class Socket {
+  _url = new URL('wss://mr2kmmhhj4.execute-api.eu-west-1.amazonaws.com/production');
+  _socket = null;
+  _handlers = [];
+  _reconnectAttempts = 0;
+  _reconnectTimeout = null;
+  _finished = false;
+  _messageFragments = [];
+
+  constructor(token, params = {}) {
+    for (const [key, value] of Object.entries({ token, ...params })) {
+      if ((value ?? null) !== null) {
+        this._url.searchParams.append(key, value);
+      }
+    }
+
+    this._connect();
+  }
+
+  onMessage(fn) {
+    this._handlers.push(fn);
+  }
+
+  async send(message) {
+    while (this._socket?.readyState !== 1) {
+      if (this._finished) return;
+
+      // eslint-disable-next-line no-await-in-loop
+      await new Promise((resolve) => {
+        setTimeout(resolve, CHECK_SOCKET_READY_INTERVAL);
+      });
+    }
+
+    this._socket.send(JSON.stringify(message));
+  }
+
+  // Close the underlying socket and remove all event listeners.
+  close() {
+    if (this._socket) {
+      this._socket.onopen = null;
+      this._socket.onclose = null;
+      this._socket.onmessage = null;
+      this._socket.close();
+      this._socket = null;
+    }
+
+    this._handlers = {};
+    this._reconnectAttempts = 0;
+    this._reconnectTimeout = null;
+    this._finished = true;
+  }
+
+  _connect = () => {
+    this._socket = new WebSocket(this._url.href);
+    this._socket.onopen = this._onSocketOpen;
+    this._socket.onclose = this._onSocketClose;
+    this._socket.onmessage = this._onSocketMessage;
+  };
+
+  _onSocketOpen = () => {
+    clearTimeout(this._reconnectTimeout);
+    this._reconnectAttempts = 0;
+  };
+
+  _onSocketClose = () => {
+    if (this._reconnectAttempts === MAX_RECONNECT_ATTEMPTS) return;
+
+    const delay = MIN_RECONNECT_DELAY * (RECONNECT_DECAY ** this._reconnectAttempts);
+
+    this._reconnectTimeout = setTimeout(() => {
+      this._reconnectAttempts++;
+      this._connect();
+    }, delay);
+  };
+
+  _onSocketMessage = ({ data }) => {
+    let message;
+
+    try {
+      message = JSON.parse(data);
+    } catch (err) {
+      return;
+    }
+
+    for (const handler of this._handlers) {
+      handler(message);
+    }
+    // const [isNotFinalFragment, fragment] = atob(data)
+    //   .match(/^(FIN=0)?([\s\S]*)$/)
+    //   .slice(1);
+
+    // this._messageFragments.push(fragment);
+    // if (isNotFinalFragment) return;
+
+    // const rawMessage = this._messageFragments.join('');
+    // this._messageFragments = [];
+
+    // let message;
+    // try {
+    //   message = JSON.parse(rawMessage);
+    // } catch (err) {
+    //   return; // Ignore malformed messages.
+    // }
+
+    // for (const handler of this._handlers[message.action] ?? []) {
+    //   handler(message.data, message.action);
+    // }
+  };
+}
+
+const socket = new Socket();
+
+// Game.
 const canvas = document.getElementById('game');
 const context = canvas.getContext('2d');
 const grid = 15;
@@ -37,6 +166,11 @@ const state = {
     dy: -ballSpeed
   },
 };
+
+socket.onMessage((data) => {
+  state.paddles[0].y = data.message.paddles[0];
+  state.paddles[1].y = data.message.paddles[1];
+});
 
 // Augment state with constant values for ease of referencing in calculations.
 // This happens here because we expect the state object to be sent down to the
@@ -180,16 +314,20 @@ function loop() {
 document.addEventListener('keydown', function(e) {
   // Up/down arrow keys.
   if (e.which === 38) {
-    rightPaddle.dy = -paddleSpeed;
+    // rightPaddle.dy = -paddleSpeed;
+    socket.send({ event: 1, paddle: 1 });
   } else if (e.which === 40) {
-    rightPaddle.dy = paddleSpeed;
+    // rightPaddle.dy = paddleSpeed;
+    socket.send({ event: -1, paddle: 1 });
   }
 
   // "W"/"A" keys.
   if (e.which === 87) {
-    leftPaddle.dy = -paddleSpeed;
+    // leftPaddle.dy = -paddleSpeed;
+    socket.send({ event: 1, paddle: 0 });
   } else if (e.which === 83) {
-    leftPaddle.dy = paddleSpeed;
+    // leftPaddle.dy = paddleSpeed;
+    socket.send({ event: -1, paddle: 0 });
   }
 });
 
